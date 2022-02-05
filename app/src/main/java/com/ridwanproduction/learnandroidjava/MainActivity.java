4a55712e8e14574415c9956beac1b9472e34a541
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2022-02-05 14:39:09 +0700

    init-project

--- app/src/main/java/com/ridwanproduction/learnandroidjava/MainActivity.java
+++ app/src/main/java/com/ridwanproduction/learnandroidjava/MainActivity.java
@@ -22,66 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate called");
         setContentView(R.layout.activity_main);
-        startServiceViaWorker();
 
         Button buttonNotif = findViewById(R.id.buttonNotif);
         buttonNotif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService();
+
             }
         });
     }
 
-    public void onStartServiceClick(View v) {
-        startService();
-    }
-
-    public void onStopServiceClick(View v) {
-        stopService();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(TAG, "onDestroy called");
-        stopService();
-        super.onDestroy();
-    }
-
-    public void startService() {
-        
-        Log.d(TAG, "startService called");
-        if (!MyService.isServiceRunning) {
-            Intent serviceIntent = new Intent(this, MyService.class);
-            ContextCompat.startForegroundService(this, serviceIntent);
-        }
-    }
-
-    public void stopService() {
-        Log.d(TAG, "stopService called");
-        if (MyService.isServiceRunning) {
-            Intent serviceIntent = new Intent(this, MyService.class);
-            stopService(serviceIntent);
-        }
-    }
-
-    public void startServiceViaWorker() {
-        Log.d(TAG, "startServiceViaWorker called");
-        String UNIQUE_WORK_NAME = "StartMyServiceViaWorker";
-        WorkManager workManager = WorkManager.getInstance(this);
-
-        // As per Documentation: The minimum repeat interval that can be defined is 15 minutes
-        // (same as the JobScheduler API), but in practice 15 doesn't work. Using 16 here
-        PeriodicWorkRequest request =
-                new PeriodicWorkRequest.Builder(
-                        MyWorker.class,
-                        16,
-                        TimeUnit.MINUTES)
-                        .build();
-
-        // to schedule a unique work, no matter how many times app is opened i.e. startServiceViaWorker gets called
-        // do check for AutoStart permission
-        workManager.enqueueUniquePeriodicWork(UNIQUE_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, request);
-
-    }
 }
